59c0961e8cbb25409e877b8d7da22a028e47dda4
Anshika Gupta <67957243+Anshika1301@users.noreply.github.com>
2021-05-07 17:50:19 +0700

    add files

---
+++ Chat_Application/src/App.js
@@ -0,0 +1,28 @@
+
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = 'f8d32fd5-6b3f-4ee5-aaed-b0e6924df065'; // anshika
+// const projectID = '16e1cedb-23a2-47bd-8bf4-3d2e58598e9f';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
